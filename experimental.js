1aab0bfcd47c37c6f0d94a736f7fe44725b85c66
Gabriel Montes <gabriel@bloq.com>
2018-03-18 16:13:58 -0300

    Create experimental set

---
+++ experimental.js
@@ -0,0 +1,15 @@
+/* eslint max-len: ["warn", { "code": 132 }] */
+
+'use strict'
+
+module.exports = {
+  rules: {
+    'consistent-return': 'warn',
+    'max-len': ['warn', { code: 80, tabWidth: 2, ignoreComments: false }],
+    'no-else-return': 'warn',
+    'no-magic-numbers': ['warn', { ignore: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16] }],
+    'no-multi-assign': 'warn',
+    'no-negated-condition': 'warn',
+    'no-param-reassign': 'warn'
+  }
+}
