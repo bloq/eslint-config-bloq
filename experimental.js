73fc54369f9a86259100fb5ac5d87ca3e0d67119
Gabriel Montes <gabriel@bloq.com>
2018-03-20 16:58:21 -0300

    Fix magic numbers rule

--- experimental.js
+++ experimental.js
@@ -7,7 +7,7 @@ module.exports = {
     'consistent-return': 'warn',
     'max-len': ['warn', { code: 80, tabWidth: 2, ignoreComments: false }],
     'no-else-return': 'warn',
-    'no-magic-numbers': ['warn', { ignore: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16] }],
+    'no-magic-numbers': ['warn', { ignore: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16] }],
     'no-multi-assign': 'warn',
     'no-negated-condition': 'warn',
     'no-param-reassign': 'warn'
