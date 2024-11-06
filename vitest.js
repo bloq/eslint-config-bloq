3ee4a2e800c21b62c0dbf9487674ba30073f2f5b
Gonzalo D'Elia <gonzalo@bloq.com>
2024-11-06 19:12:22 -0300

    Fix name of vitest rules

--- vitest.js
+++ vitest.js
@@ -4,14 +4,14 @@ module.exports = {
   extends: ['plugin:@vitest/legacy-recommended'],
   plugins: ['@vitest'],
   rules: {
+    // equivalent to mocha/no-exclusive-tests
+    '@vitest/no-focused-tests': 'error',
+    // equivalent to mocha/no-setup-in-describe
+    '@vitest/require-hook': 'warn',
+    // equivalent to mocha/no-skipped-tests
+    '@vitest/no-disabled-tests': 'error',
     'no-unused-expressions': 'off',
     'node/no-unpublished-require': 'warn',
-    'prefer-arrow/prefer-arrow-functions': 'off',
-    // equivalent to mocha/no-exclusive-tests
-    'vitest/no-focused-tests': 'error',
-    // equivalent to mocha/no-setup-in-describe
-    'vitest/require-hook': 'warn',
-    // equivalent to mocha/no-skipped-tests
-    'vitest/no-disabled-tests': 'error'
+    'prefer-arrow/prefer-arrow-functions': 'off'
   }
 }
