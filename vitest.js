e53e0620dd14a4834989b390ae9e0f3503306ef4
Gonzalo D'Elia <gonzalo@bloq.com>
2024-11-07 13:06:41 -0300

    Sort keys in vitest.js

--- vitest.js
+++ vitest.js
@@ -4,11 +4,11 @@ module.exports = {
   extends: ['plugin:@vitest/legacy-recommended'],
   plugins: ['@vitest'],
   rules: {
+    // equivalent to mocha/no-skipped-tests
+    '@vitest/no-disabled-tests': 'error',
     // equivalent to mocha/no-exclusive-tests
     '@vitest/no-focused-tests': 'error',
     // equivalent to mocha/no-setup-in-describe
-    '@vitest/require-hook': 'warn',
-    // equivalent to mocha/no-skipped-tests
-    '@vitest/no-disabled-tests': 'error'
+    '@vitest/require-hook': 'warn'
   }
 }
