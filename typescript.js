2116c70c104ff4dd06f9c351422bb5fe2e7d4ab7
Gonzalo D'Elia <gonzalo@bloq.com>
2025-06-12 19:45:29 -0300

    Set no-unused-vars with ignoreRestSiblings

--- typescript.js
+++ typescript.js
@@ -1,5 +1,13 @@
 'use strict'
 
 module.exports = {
-  extends: ['plugin:@typescript-eslint/recommended']
+  extends: ['plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true
+      }
+    ]
+  }
 }
