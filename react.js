eeab9293d14ddcb8866c111c1119ec2de568f159
Gonzalo D'Elia <gonzalo@bloq.com>
2026-05-11 15:27:24 -0300

    Use react-hooks/recommended-legacy preset
    The default `recommended` preset in eslint-plugin-react-hooks v5+
    ships as a flat config and is incompatible with this package's
    eslintrc-style configuration. Switch to the `recommended-legacy`
    preset, which retains the legacy eslintrc format.
    
    Fixes #67

--- react.js
+++ react.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended-legacy',
     'plugin:jsx-a11y/recommended'
   ],
   parserOptions: {
